473cd4b69d28f6d185a19cc55c192a14dacbe2a9
Ranshi <782938005@qq.com>
2021-08-25 14:58:17 +0800

    2021.8.25

---
+++ meituan/001/main.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+# @Time     : 2021/08/25 13:45
+# @Author   : Ranshi
+# @File     : main.py
+
+t = int(input())
+for _ in range(t):
+    usr = input()
+    if not usr[0].isalpha() or not usr.isalnum():
+        print('Wrong')
+    else:
+        if not usr.isalpha() and not usr.isdigit():
+            print('Accept')
+        else:
+            print('Wrong')
---
+++ meituan/002/main.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# @Time     : 2021/08/25 14:20
+# @Author   : Ranshi
+# @File     : main.py
+
