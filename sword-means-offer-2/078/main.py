b7b713142ea8099ca401154eb7b46931834da416
Ranshi <ranshi159@icloud.com>
2023-01-30 23:49:06 +0000

    2023.1.30

---
+++ sword-means-offer-2/078/main.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+# @Time     : 2023/01/22 22:50
+# @Author   : Ranshi
+# @File     : main.py
+# @Doc      : 剑指 Offer II 078. 合并排序链表
+from typing import List
+from type.list_node import ListNode
+
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        dummy_head = ListNode()
+        p = dummy_head
+        while True:
+            _min = 0
+            for i in range(0, len(lists)):
+                if lists[_min] is None or (
+                    lists[i] is not None and lists[_min].val > lists[i].val
+                ):
+                    _min = i
+            if lists[_min] is None:
+                break
+            p.next = lists[_min]
+            lists[_min].next, lists[_min] = None, lists[_min].next
+        return dummy_head.next
+
+
+if __name__ == "__main__":
+    print(
+        Solution().mergeKLists(
+            [
+                ListNode.init_by_lst([1, 4, 5]),
+                ListNode.init_by_lst([1, 3, 4]),
+                ListNode.init_by_lst([2, 6]),
+            ]
+        )
+    )
